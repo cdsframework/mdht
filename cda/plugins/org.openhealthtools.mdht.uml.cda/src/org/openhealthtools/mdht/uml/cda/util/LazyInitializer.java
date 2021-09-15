0586003105d22ae50898bd0baf59a3f8389bc729
Blake <bminghelli@gmail.com>
2021-09-15 14:54:56 -0700

    Fixed race condition vulnerability caused by unsychronized initialization of the OCL query expressions. Under the right race condition, it can cause the static OCL variables to be left in a corrupt state and indefinitely broken. RCKMSDEV-456

---
+++ cda/plugins/org.openhealthtools.mdht.uml.cda/src/org/openhealthtools/mdht/uml/cda/util/LazyInitializer.java
@@ -0,0 +1,49 @@
+package org.openhealthtools.mdht.uml.cda.util;
+
+/**
+ * Copy of LazyInitializer from apache commons-lang because I didn't want to pull in the whole aapche lib just
+ * for this. Only change is the removal of the use of ConcurrentException (initialize() doesn't throw it)
+ * @param <T>
+ */
+public abstract class LazyInitializer<T> {
+  private static final Object NO_INIT = new Object();
+
+  @SuppressWarnings("unchecked")
+  // Stores the managed object.
+  private volatile T object = (T) NO_INIT;
+
+  /**
+   * Returns the object wrapped by this instance. On first access the object
+   * is created. After that it is cached and can be accessed pretty fast.
+   *
+   * @return the object initialized by this {@code LazyInitializer}
+   */
+  public T get() {
+    // use a temporary variable to reduce the number of reads of the
+    // volatile field
+    T result = object;
+
+    if (result == NO_INIT) {
+      synchronized (this) {
+        result = object;
+        if (result == NO_INIT) {
+          object = result = initialize();
+        }
+      }
+    }
+
+    return result;
+  }
+
+  /**
+   * Creates and initializes the object managed by this {@code
+   * LazyInitializer}. This method is called by {@link #get()} when the object
+   * is accessed for the first time. An implementation can focus on the
+   * creation of the object. No synchronization is needed, as this is already
+   * handled by {@code get()}.
+   *
+   * @return the managed data object
+   */
+  protected abstract T initialize();
+
+}
